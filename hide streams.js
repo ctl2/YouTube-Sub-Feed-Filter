644fee1f1a4d30092e2cd478084b7db3bed3172d
CallumL <ctl2@hw.ac.uk>
2020-10-19 15:39:39 +0100

    Update hide streams.js

--- hide streams.js
+++ hide streams.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       Hide YouTube Streams
 // @version    0.1
-// @description Hides streams from the YouTube subscriptions page
+// @description Hides streams (and premiers) from the YouTube subscriptions page.
 // @match      http://www.youtube.com/feed/subscriptions*
 // @match      http://youtube.com/feed/subscriptions*
 // @match      https://www.youtube.com/feed/subscriptions*
@@ -26,66 +26,128 @@ let hideConfig = {
     }
 };
 
-function getFirstWord(element) {
-    return element.innerText.split(" ")[0];
-}
-
-function hideScheduledStreams(videos) {
-    // TODO
-}
-
-function hideLiveStreams() {
-    for (let badge of document.getElementsByClassName("badge-style-type-live-now")) {
-        badge.parentElement.parentElement.parentElement.parentElement.style.display = "none"
+function getFilteredCollection(collection, predicate) {
+    let filteredCollection = [];
+    for (let item of collection) {
+        if (predicate(item)) filteredCollection.push(item);
     }
+    return filteredCollection;
 }
 
-function hideFinishedStreams(videos) {
-    for (let video of videos) {
-        if (getFirstWord(video.querySelector("#metadata-line").children[1]) === "Streamed") video.style.display = "none"
-    }
+function firstWordEquals(element, word) {
+    return element.innerText.split(" ")[0] === word;
 }
 
-function hideScheduledPremiers(videos) {
-    for (let video of videos) {
-        if (getFirstWord(video.querySelector("#metadata-line").children[0]) === "Premieres") video.style.display = "none"
-    }
+function getLiveBadge(video) {
+    return video.querySelector(".badge-style-type-live-now");
 }
 
-function hideLivePremiers(videos) {
-    // TODO
+function getMetadataLine(video) {
+    return video.querySelector("#metadata-line");
 }
+
+function hide(element) {
+    element.style.display = "none";
+}
+
+// Hide scheduled
+
+function getScheduledStreams(videos) {
+    return getFilteredCollection(
+        videos,
+        video => firstWordEquals(getMetadataLine(video).children[0], "Scheduled")
+    );
+}
+
+function getScheduledPremiers(videos) {
+    return getFilteredCollection(
+        videos,
+        video => firstWordEquals(getMetadataLine(video).children[0], "Premieres")
+    );
+}
+
+// Hide live
+
+function getLiveStreams(videos) {
+    return getFilteredCollection(
+        videos,
+        video => {
+            let liveBadge = getLiveBadge(video);
+            return liveBadge === null ?
+                false :
+                firstWordEquals(liveBadge.querySelector("span.ytd-badge-supported-renderer"), "LIVE");
+        }
+    );
+}
+
+function getLivePremiers(videos) {
+    return getFilteredCollection(
+        videos,
+        video => {
+            let liveBadge = getLiveBadge(video);
+            return liveBadge === null ?
+                false :
+                firstWordEquals(liveBadge.querySelector("span.ytd-badge-supported-renderer"), "PREMIERING");
+        }
+    );
+}
+
+// Hide finished
+
+function getFinishedStreams(videos) {
+    return getFilteredCollection(
+        videos,
+        video => firstWordEquals(getMetadataLine(video).children[1], "Streamed")
+    );
+}
+
+// Hide controller
 
 function hideStreams(newMutations) {
-    // Collect videos
-    let videos;
-    if (newMutations == null) {
-        videos = document.querySelectorAll('ytd-grid-video-renderer');
+    // Collect new video sections
+    let videoSections; // Today, This week, This month, ...
+    if (newMutations === undefined) {
+        videoSections = document.querySelectorAll("ytd-item-section-renderer");
     } else {
-        videos = [];
+        videoSections = [];
         for (let mutation of newMutations) {
             for (let addedNode of mutation.addedNodes) {
-                videos = videos.concat(...addedNode.querySelectorAll("ytd-grid-video-renderer"));
+                videoSections.push(addedNode);
             }
         }
     }
-    // Hide streams
-    if (hideConfig.scheduled.streams === true) hideScheduledStreams(videos);
-    if (hideConfig.live.streams === true) hideLiveStreams();
-    if (hideConfig.finished.streams === true) hideFinishedStreams(videos);
-    // Hide premiers
-    if (hideConfig.scheduled.premiers === true) hideScheduledPremiers(videos);
-    if (hideConfig.live.premiers === true) hideLivePremiers();
+    for (let videoSection of videoSections) {
+        // Collect new videos
+        let videos = videoSection.querySelectorAll("ytd-grid-video-renderer");
+        let hideableVideos = [];
+        // Hide scheduled
+        if (hideConfig.scheduled.streams === true) hideableVideos = hideableVideos.concat(getScheduledStreams(videos));
+        if (hideConfig.scheduled.premiers === true) hideableVideos = hideableVideos.concat(getScheduledPremiers(videos));
+        // Hide live
+        if (hideConfig.live.streams === true) hideableVideos = hideableVideos.concat(getLiveStreams(videos));
+        if (hideConfig.live.premiers === true) hideableVideos = hideableVideos.concat(getLivePremiers(videos));
+        // Hide finished
+        if (hideConfig.finished.streams === true) hideableVideos = hideableVideos.concat(getFinishedStreams(videos));
+        if (hideableVideos.length === videos.length) {
+            hide(videoSection);
+        } else {
+            for (let hideableVideo of hideableVideos) {
+                hide(hideableVideo);
+            }
+        }
+    }
 }
 
-var observer = new MutationObserver(hideStreams)
+// Call the hideStreams function when new videos are loaded
+
+var observer = new MutationObserver(hideStreams);
 observer.observe(
     document.querySelector('div#contents'), {
         childList: true,
-        subtree: false, 
-        attributes: false, 
-        characterData : false
+        subtree: false,
+        attributes: false,
+        characterData: false
     }
-)
+);
 
 hideStreams();
